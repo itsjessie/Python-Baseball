dc8350ed162ac7989a455f6218f71a571c1a0805
itsjessie <44765634+itsjessie@users.noreply.github.com>
2019-05-03 16:33:55 +1000

    Update pitching.py

--- stats/pitching.py
+++ stats/pitching.py
@@ -1,2 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from data import games
+#print(games)
+
+#don't need to use .loc
+plays = games[games['type']=='play']
+
+#print (plays)
+
+strike_outs=plays[plays['event'].str.contains('K')]
+#print (strike_outs)
+
+strike_outs= strike_outs.groupby(['year','game_id']).size()
+
+strike_outs= strike_outs.reset_index(name= 'strike_outs')
+
+strike_outs = strike_outs.loc[:,['year','strike_outs']].apply(pd.to_numeric)
+#print (strike_outs)
+
+strike_outs.plot(x='year',y='strike_outs',kind= 'scatter').legend(['Strike Outs'])
+
+plt.show()
