6badd0584f192b423ec29d439bd310f9e4921c2b
itsjessie <44765634+itsjessie@users.noreply.github.com>
2019-05-03 11:32:05 +1000

    Update attendance.py

--- stats/attendance.py
+++ stats/attendance.py
@@ -17,6 +17,6 @@ attendance.plot( x='year',y='attendance',figsize= (15,7), kind='bar')
 plt.xlabel('Year')
 plt.ylabel('Attendance')
 
-plt.axhline( label= 'Mean', y = attendance['attendance'].mean(), color= 'green', linestyle= 'dashed')
+plt.axhline( label= 'Mean', y = attendance['attendance'].mean(), color= 'green', linestyle= '--')
 
 plt.show()
